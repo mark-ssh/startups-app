0d536e7e962a043ea3df46afabcfed6fc6874735
Mark Wonah <markwonah@gmail.com>
2024-12-03 16:47:11 +0100

    added Authentication

---
+++ app/component/Navbar.tsx
@@ -0,0 +1,53 @@
+import { auth, signIn, signOut } from "@/auth";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = async () => {
+  const session = await auth();
+  const logout = async () => {
+    "use server";
+
+    await signOut();
+  };
+  const login = async () => {
+    "use server";
+
+    await signIn("github");
+  };
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/" className="w-fit">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create" className="text-gray-600">
+                <span>Create</span>
+              </Link>
+              <form action={logout} className="text-red-800">
+                <button type="submit">Logout</button>
+              </form>
+
+              <Link
+                href={`/user/${session?.id}`}
+                className="text-gray-800 font-semibold"
+              >
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form action={login}>
+              <button type="submit" className="text-gray-800">
+                Login
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- app/dashboard/users/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const page = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
-
-  return <div className="text-3xl">USER PROFILE: {id} </div>;
-};
-
-export default page;
